9406700225d710f08ff3b9c571423dc334e898e4
Titirat Khunsap <113292765+pewyves@users.noreply.github.com>
2023-07-06 12:56:47 +0700

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import './App.css';
 function Footer() {
     return (
         <div className='MyFooter'>
-            <span>Copyright 2023 @ Suvalak Namsaeng</span>
+            <span>Copyright 2023 @ Titirat Khunsap</span>
         </div>
     );
 }
